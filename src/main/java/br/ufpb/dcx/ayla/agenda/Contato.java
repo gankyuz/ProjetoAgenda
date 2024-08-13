2b646d2998ee0166f075cac8f9557ab341e17b9f
gabriela.oliveira <gabisoliporto@gmail.com>
2024-08-13 09:08:30 -0300

    Iniciando Projeto

---
+++ src/main/java/br/ufpb/dcx/ayla/agenda/Contato.java
@@ -0,0 +1,18 @@
+package br.ufpb.dcx.ayla.agenda;
+
+public class Contato {
+
+    private String nome;
+    private int diaAniversario;
+    private int mesAniversario;
+
+    public Contato(String nome, int diaAniversario, int mesAniversario) {
+        this.nome = nome;
+        this.diaAniversario = diaAniversario;
+        this.mesAniversario = mesAniversario;
+    }
+
+    public Contato() {
+        this("", 0, 0);
+    }
+}
