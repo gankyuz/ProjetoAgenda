32a7602bc4a45ba760b1f13eb5ed23074ec1e2ee
gabriela.oliveira <gabisoliporto@gmail.com>
2024-08-15 10:20:22 -0300

    att

--- src/main/java/br/ufpb/dcx/ayla/agenda/ProgramaAgenda.java
+++ src/main/java/br/ufpb/dcx/ayla/agenda/ProgramaAgenda.java
@@ -6,39 +6,40 @@ import java.io.IOException;
 public class ProgramaAgenda {
     public static void main(String[] args) {
         Agenda a2 = new AgendaAyla();
+
         try{
             a2.recuperarDados();
-            boolean continuar = true;
-            while (continuar){
-                String opcao = JOptionPane.showInputDialog("1.Cadastrar\n2.Pesquisar\n3.Remover\n4.Sair");
-                switch (opcao) {
-                    case "1":
-                        String nome = JOptionPane.showInputDialog("Nome");
-                        int dia = Integer.parseInt(JOptionPane.showInputDialog("Dia:"));
-                        int mes = Integer.parseInt(JOptionPane.showInputDialog("Mês:"));
-                        a2.cadastraContato(nome, dia, mes);
-                        break;
-                    case "2":
-                        int dia2 = Integer.parseInt(JOptionPane.showInputDialog("Dia:"));
-                        int mes2 = Integer.parseInt(JOptionPane.showInputDialog("Mês:"));
-                        JOptionPane.showMessageDialog(null,a2.pesquisaAniversariantes(dia2,mes2).size());
-                        break;
-                    case "3":
-                        String nomeRemove = JOptionPane.showInputDialog("Nome");
-                        try{
-                            a2.removeContato(nomeRemove);
-                        }catch (ContatoInexistenteException e){
-                            JOptionPane.showMessageDialog(null, e.getMessage());
-                        }
-                        break;
-                    case "4":
-                        continuar = false;
-                        break;
-                }
-            }
-            a2.salvarDados();
         }catch (IOException e){
             e.printStackTrace();
+        }
+
+        boolean continuar = true;
+        while (continuar) {
+            String opcao = JOptionPane.showInputDialog("1.Cadastrar\n2.Pesquisar\n3.Remover\n4.Sair");
+            switch (opcao) {
+                case "1":
+                    String nome = JOptionPane.showInputDialog("Nome");
+                    int dia = Integer.parseInt(JOptionPane.showInputDialog("Dia:"));
+                    int mes = Integer.parseInt(JOptionPane.showInputDialog("Mês:"));
+                    a2.cadastraContato(nome, dia, mes);
+                    break;
+                case "2":
+                    int dia2 = Integer.parseInt(JOptionPane.showInputDialog("Dia:"));
+                    int mes2 = Integer.parseInt(JOptionPane.showInputDialog("Mês:"));
+                    JOptionPane.showMessageDialog(null, a2.pesquisaAniversariantes(dia2, mes2).size());
+                    break;
+                case "3":
+                    String nomeRemove = JOptionPane.showInputDialog("Nome");
+                    try {
+                        a2.removeContato(nomeRemove);
+                    } catch (ContatoInexistenteException e) {
+                        JOptionPane.showMessageDialog(null, e.getMessage());
+                    }
+                    break;
+                case "4":
+                    continuar = false;
+                    break;
+            }
         }
     }
 }
